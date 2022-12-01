507b5fb7c495230597ef29b295f023391fa2f162
Chris Paul <cpaul@squareup.com>
2022-12-01 10:38:27 -0800

    Day 1, part 2 complete

--- day1.rb
+++ day1.rb
@@ -26,6 +26,38 @@ def most_calories(elf_calories)
     max_calories
 end
 
+def top_total_calories(elf_calories, num_top = 3)
+    max_calories = []
+
+    current_calories = 0
+    current_elf_index = 0
+
+    elf_calories.map(&:to_i).each_with_index.map do |value, index|
+        if index == elf_calories.count - 1
+            current_calories += value
+        end
+        if index == elf_calories.count - 1 || value == 0
+            if max_calories.count < num_top
+                max_calories.push(current_calories)
+            else
+                lowest_index = max_calories.index { |v| v < current_calories }
+                max_calories[lowest_index] = current_calories unless lowest_index.nil?
+            end
+
+            # Sorting ensures the lowest value is replaced
+            max_calories.sort!
+
+            current_calories = 0
+            current_elf_index += 1
+            next
+        end
+
+        current_calories += value
+    end
+
+    max_calories.sum
+end
+
 # Test input, should result in 4th elf who carries 24000 calories
 # elf_calories = [
 #     "1000\n",
@@ -45,4 +77,9 @@ end
 # ]
 
 elf_calories = File.readlines('day1-input.txt')
-puts most_calories(elf_calories)
+
+# Part 1
+# puts most_calories(elf_calories)
+
+# Part 2
+puts top_total_calories(elf_calories)
