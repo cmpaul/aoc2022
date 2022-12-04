a793e6c13bdcf6446f7aaa58dc8091bcc156f5b7
Chris Paul <cpaul@squareup.com>
2022-12-04 06:33:25 -0800

    Day 4, part 1 complete

--- day4.rb
+++ day4.rb
@@ -8,6 +8,14 @@ def fully_contained?(assignment)
         second_lower >= first_lower && second_upper <= first_upper
 end
 
+def overlap?(assignment)
+    first, second = assignment.strip.split(',')
+    first_lower, first_upper = first.split('-').map(&:to_i)
+    second_lower, second_upper = second.split('-').map(&:to_i)
+    return first_lower >= second_lower && first_lower <= second_upper ||
+        first_upper >= second_lower && first_upper <= second_upper
+end
+
 # assignments = %w[
 #     2-4,6-8
 #     2-3,4-5
@@ -18,4 +26,8 @@ end
 # ]
 assignments = File.readlines('day4-input.txt')
 
+# part 1
+# puts assignments.select { |assignment| fully_contained?(assignment) }.size
+
+# part 2
 puts assignments.select { |assignment| fully_contained?(assignment) }.size
