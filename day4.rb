72b68854d6dffebad2d930f1c176510562d33ce2
Chris Paul <cpaul@squareup.com>
2022-12-04 06:30:36 -0800

    Day 4, part 1 complete

---
+++ day4.rb
@@ -0,0 +1,21 @@
+# --- Day 4: Camp Cleanup ---
+
+def fully_contained?(assignment)
+    first, second = assignment.strip.split(',')
+    first_lower, first_upper = first.split('-').map(&:to_i)
+    second_lower, second_upper = second.split('-').map(&:to_i)
+    return first_lower >= second_lower && first_upper <= second_upper ||
+        second_lower >= first_lower && second_upper <= first_upper
+end
+
+# assignments = %w[
+#     2-4,6-8
+#     2-3,4-5
+#     5-7,7-9
+#     2-8,3-7
+#     6-6,4-6
+#     2-6,4-8
+# ]
+assignments = File.readlines('day4-input.txt')
+
+puts assignments.select { |assignment| fully_contained?(assignment) }.size
