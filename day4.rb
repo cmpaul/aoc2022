bce9c6ae56b68ee0ebde5538966a1b244618c05f
Chris Paul <cpaul@squareup.com>
2022-12-04 07:09:33 -0800

    Day 4, part 2 complete

--- day4.rb
+++ day4.rb
@@ -12,22 +12,29 @@ def overlap?(assignment)
     first, second = assignment.strip.split(',')
     first_lower, first_upper = first.split('-').map(&:to_i)
     second_lower, second_upper = second.split('-').map(&:to_i)
-    return first_lower >= second_lower && first_lower <= second_upper ||
-        first_upper >= second_lower && first_upper <= second_upper
+
+    # completely out of bounds
+    return false if second_lower > first_upper || second_upper < first_lower
+
+    # any overlap?
+    return (first_lower >= second_lower && first_lower <= second_upper) ||
+        (first_upper >= second_lower && first_upper >= second_upper) ||
+        (second_lower >= first_lower && second_lower <= first_upper) ||
+        (second_upper >= first_lower && second_upper <= first_upper)
 end
 
-# assignments = %w[
-#     2-4,6-8
-#     2-3,4-5
-#     5-7,7-9
-#     2-8,3-7
-#     6-6,4-6
-#     2-6,4-8
-# ]
+assignments = %w[
+    2-4,6-8
+    2-3,4-5
+    5-7,7-9
+    2-8,3-7
+    6-6,4-6
+    2-6,4-8
+]
 assignments = File.readlines('day4-input.txt')
 
 # part 1
 # puts assignments.select { |assignment| fully_contained?(assignment) }.size
 
 # part 2
-puts assignments.select { |assignment| fully_contained?(assignment) }.size
+puts assignments.select { |assignment| overlap?(assignment) }.size
