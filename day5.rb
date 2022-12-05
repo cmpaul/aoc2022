be1cd119808f7244a14f0de4050c776fce5a0057
Chris Paul <cpaul@squareup.com>
2022-12-04 21:46:14 -0800

    Day 5, part 1 complete

--- day5.rb
+++ day5.rb
@@ -8,10 +8,9 @@ def build_stacks(input)
         return unless stack_line.strip.start_with?('[')
 
         crates = stack_line.chars.each_slice(4).map(&:join)
-        puts "crates = #{crates}"
         crates.each_with_index do |crate, index|
             next if crate.strip.empty?
-            puts "#{index} = #{crate}"
+
             stack = STACKS[index].nil? ? [] : STACKS[index]
             stack.push(crate[1])
             STACKS[index] = stack
@@ -19,8 +18,29 @@ def build_stacks(input)
     end
 end
 
+def move_stacks(input)
+    for i in 0..(input.size - 1) do
+        instruction = input[i].strip
+        next unless instruction.start_with?("move")
+
+        puts "instruction = #{instruction}"
+        count, orig, dest = instruction.match(/move (\d+) from (\d+) to (\d+)/).captures.map(&:to_i)
+        puts "count = #{count}, orig = #{orig}, dest = #{dest}"
+        orig_stack = STACKS[orig - 1]
+        dest_stack = STACKS[dest - 1]
+        for num in 1..count do
+            moved = orig_stack.shift
+            dest_stack.unshift(moved)
+        end
+        puts STACKS.inspect
+        puts
+        # STACKS[orig - 1] = orig_stack
+        # STACKS[dest - 1] = dest_stack
+    end
+end
+
 input = [
-    "[D]        \n",
+    "    [D]    \n",
     "[N] [C]    \n",
     "[Z] [M] [P]\n",
     "1   2   3  \n",
@@ -31,7 +51,12 @@ input = [
     "move 1 from 1 to 2\n",
 ]
 
-# input = File.readlines('day5-input.txt')
+input = File.readlines('day5-input.txt')
 
 build_stacks(input)
 puts STACKS.inspect
+
+move_stacks(input)
+puts STACKS.inspect
+
+puts STACKS.map { |row| row.shift }.join
